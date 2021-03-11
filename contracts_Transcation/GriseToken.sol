eeb9136a09fc03e329db03c07aef55cd6260b51c
unblocktechie <76834820+unblocktechie@users.noreply.github.com>
2021-03-11 04:47:10 -0800

    Add files via upload
    Add tokenmatrix gatekeeper

--- contracts_Transcation/GriseToken.sol
+++ contracts_Transcation/GriseToken.sol
@@ -311,7 +311,7 @@ contract GriseToken is Utils {
             account != address(0x0)
         );
 
-        // amount = amount.mul(TEMP_PRECISION); // VIJAY
+        amount = amount.mul(TEMP_PRECISION);
         
         _balances[account] =
         _balances[account].sub(amount);
@@ -638,5 +638,9 @@ contract GriseToken is Utils {
         _days = currentGriseDay().mod(GRISE_WEEK) > 0 ?
                     GRISE_WEEK - currentGriseDay().mod(GRISE_WEEK) :
                     0;
+    }
+
+    function balanceOfStaker(address account) external view returns (uint256) {
+        return _balances[account];
     }
 }
