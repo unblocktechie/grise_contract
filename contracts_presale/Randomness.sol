f52a124c218f9a46b60fea5ee1bec8da10490211
unblocktechie <76834820+unblocktechie@users.noreply.github.com>
2021-03-11 16:16:51 +0530

    Update Randomness.sol

--- contracts_presale/Randomness.sol
+++ contracts_presale/Randomness.sol
@@ -4,10 +4,12 @@ pragma solidity =0.7.6;
 import "./nreAPI.sol";
 
 contract Randomness is usingNRE {
-    uint256 public randomNumber;    
+    //uint256 public randomNumber;    
     
-   function stateRandomNumber() public {
+   function stateRandomNumber() public returns (uint256) {
+       uint256 randomNumber;
        randomNumber = (rm()%(10**5));
+       return randomNumber;
       //randomNumber = (((rv()%10)*10000)+((rd()%10)*1000)+((rf()%10)*100)+((rx()%10)*10)+(rm()%10));
    }
  
