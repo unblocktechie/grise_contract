be7a5a45d07135f451fd8522eb8484390dafc416
unblocktechie <76834820+unblocktechie@users.noreply.github.com>
2021-04-26 17:17:47 +0530

    change bounty percentage

--- contracts_presale/LiquidityTransformer.sol
+++ contracts_presale/LiquidityTransformer.sol
@@ -683,7 +683,7 @@ contract LiquidityTransformer {
         uint256 _balance = g.totalWeiContributed;
         uint256 _buffer = g.totalTransferTokens + g.totalReferralTokens;
         
-        uint256 _bounty = _buffer.mul(5).div(100);
+        uint256 _bounty = _buffer.mul(8).div(100);
 
         _balance = _balance.sub(
             _teamContribution(
@@ -695,7 +695,7 @@ contract LiquidityTransformer {
             g.totalWeiContributed
         );
         
-        _bounty = _bounty.add(_buffer.mul(5).div(100));
+        _bounty = _bounty.add(_buffer.mul(8).div(100));
         
 
         GRISE_CONTRACT.mintSupply(
